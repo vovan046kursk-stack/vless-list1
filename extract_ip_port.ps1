dd70b0118d4d467445c6b07f33c1d58e42eed211
vovan046kursk-stack <vovan046kursk@gmail.com>
2026-02-20 23:52:22 +0300

    Update extract_ip_port.ps1

--- extract_ip_port.ps1
+++ extract_ip_port.ps1
@@ -3,9 +3,9 @@ if (Test-Path "ip_port_pool.txt") {
 }
 
 $allowedPairs = @(
-"51.250.0.147:443",
+"109.120.190.78:443",
 "109.120.191.46:443",
-"84.23.52.70:443"
+"5.188.143.111:443"
 )
 
 $allowedPairs | Set-Content ip_port_pool.txt -Encoding UTF8
