c129cf8a1c3968150fb10d1a3ef802b55afc08f2
vovan046kursk-stack <vovan046kursk@gmail.com>
2026-02-20 20:17:36 +0300

    Create download_sources.ps1

---
+++ download_sources.ps1
@@ -0,0 +1,26 @@
+$urls = @(
+"https://raw.githubusercontent.com/zieng2/wl/main/vless_universal.txt",
+"https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile.txt",
+"https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-checked.txt"
+)
+
+$out = "all_vless.txt"
+$result = @()
+
+foreach ($url in $urls) {
+    try {
+        Write-Host "Downloading $url"
+        $content = Invoke-WebRequest -Uri $url -UseBasicParsing
+        $lines = $content.Content -split "`n"
+        $vless = $lines | Where-Object { $_ -match "^vless://" }
+        $result += $vless
+    }
+    catch {
+        Write-Host "Failed: $url"
+    }
+}
+
+$result = $result | Sort-Object -Unique
+$result | Set-Content $out -Encoding utf8NoBOM
+
+Write-Host "Downloaded total:" $result.Count
