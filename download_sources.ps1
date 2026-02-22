4cde287616e2c60bb6e197ca596ece8b06dc0f51
vovan046kursk-stack <vovan046kursk@gmail.com>
2026-02-22 13:58:50 +0300

    Update download_sources.ps1

--- download_sources.ps1
+++ download_sources.ps1
@@ -1,7 +1,5 @@
 $urls = @(
-"https://raw.githubusercontent.com/zieng2/wl/main/vless_universal.txt",
-"https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile.txt",
-"https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-checked.txt"
+"https://raw.githubusercontent.com/zieng2/wl/main/vless_universal.txt"
 )
 
 $out = "all_vless.txt"
