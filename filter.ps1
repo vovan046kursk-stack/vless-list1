87c813b249812e7556f753b9f2f69d8149619895
vovan046kursk-stack <vovan046kursk@gmail.com>
2026-03-16 21:19:41 +0300

    Update filter.ps1

--- filter.ps1
+++ filter.ps1
@@ -79,7 +79,7 @@ foreach ($line in $filtered) {
 Write-Host "After whitelist:" $result.Count
 
 # ===== LIMIT OUTPUT =====
-$final = $result | Select-Object -First 100
+$final = $result | Select-Object -First 50
 $final | Set-Content $outputFile
 
 Write-Host "Final count saved:" $final.Count
