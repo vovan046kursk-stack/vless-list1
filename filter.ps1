088870ed4804dcef253c557f03366ac51823991e
vovan046kursk-stack <vovan046kursk@gmail.com>
2026-03-29 14:46:46 +0300

    Update filter.ps1

--- filter.ps1
+++ filter.ps1
@@ -38,12 +38,10 @@ foreach ($line in $lines) {
 
     # 🔥 SNI фильтр
     if (
-    $line -notmatch "api-maps\.yandex\.ru" -and
     $line -notmatch "ads\.x5\.ru" -and
-    $line -notmatch "rutube\.ru" -and
     $line -notmatch "5post-gate\.x5\.ru" -and
-    $line -notmatch "anti-vpn\.ru" -and
     $line -notmatch "x5\.ru" -and
+    $line -notmatch "anti-vpn\.ru" -and
     $line -notmatch "event.yandex\.ru" -and
     $line -notmatch "5post-gate-test\.ru" -and
     $line -notmatch "max\.ru" -and
