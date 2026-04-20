1da84ffd182aeb5e762bb0e6dfa831630d3b59b1
vovan046kursk-stack <vovan046kursk@gmail.com>
2026-04-21 00:13:18 +0300

    Update filter.ps1

--- filter.ps1
+++ filter.ps1
@@ -71,7 +71,7 @@ foreach ($line in $lines) {
     # ✅ SNI ФИЛЬТР
     # =========================
 
-    if ($line -notmatch '(5ka-cdn\.x5static\.net|ads\.x5\.ru|ads\.x5media\.ru|5post-gate\.x5\.ru|5post-gate-test\.ru|x5\.ru|max\.ru|web\.max\.ru|ru-portal\.meetvideo\.ru|www\.vk\.com|sun6-\d+\.userapi\.com|yandex\.ru|api-maps\.yandex\.ru|mc\.yandex\.ru|www\.philips\.com|wl-\d+-\d+\.legendary-vpn\.com|tunnel\.vk-apps\.com|megafon\.ru)') {
+    if ($line -notmatch '(5ka-cdn\.x5static\.net|ads\.x5\.ru|ads\.x5media\.ru|5post-gate\.x5\.ru|5post-gate-test\.ru|x5\.ru|max\.ru|web\.max\.ru|ru-portal\.meetvideo\.ru|www\.vk\.com|sun6-\d+\.userapi\.com|yandex\.ru|api-maps\.yandex\.ru|mc\.yandex\.ru|www\.philips\.com|wl-\d+-\d+\.legendary-vpn\.com|tunnel\.vk-apps\.com|megafon\.ru|vkvideo\.ru)') {
         continue
     }
 
